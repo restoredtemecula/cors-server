0cf8673e21b4abc6f9bcb3a128c64bd6a7231ae3
ogballbuster <mark@restoredtemecula.com>
2022-11-13 00:05:56 -0800

    init commit

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,25 @@
-const { onRequest } = require('firebase-functions/v2/https');
-const corsAnywhere = require('cors-anywhere');
-const cors = require('cors');
+const { onRequest } = require("firebase-functions/v2/https");
+const corsAnywhere = require("cors-anywhere");
+const cors = require("cors");
 
 const corsServer = corsAnywhere.createServer({
-    originWhitelist: [
-      'http://localhost:3000',
-      'http://localhost:5000',
-      'https://portfolio-ravenous.web.app',
-      'https://portfolio-ravenous.firebaseapp.com'
-    ],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+  originWhitelist: [
+    "http://localhost:3000",
+    "http://localhost:5000",
+    "https://www.restoredtemecula.church",
+    "https://www.restoredtemecula.com",
+    "https://restoredtemecula.church",
+    "https://restoredtemecula.com",
+    "https://portfolio-ravenous.firebaseapp.com",
+  ],
+  requireHeader: ["origin", "x-requested-with"],
+  removeHeaders: ["cookie", "cookie2"],
 });
 
 const corsHandler = cors({ origin: true });
 
 exports.proxy = onRequest((request, response) => {
-    corsHandler(request, response, () => {
-      corsServer.emit('request', request, response);
-    })
+  corsHandler(request, response, () => {
+    corsServer.emit("request", request, response);
+  });
 });
